2dc716f7fd21953318153e20b0070b82b1a3fdfb
crowdbotics-dev <engineering@crowdbotics.com>
2022-12-06 04:43:37 +0000

    Update models

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Addressxqawyyrljh
+admin.site.register(Addressxqawyyrljh)
 
 # Register your models here.
--- home/models.py
+++ home/models.py
@@ -1,1 +1,5 @@
+from django.conf import settings
 from django.db import models
+class Addressxqawyyrljh(models.Model):
+    'Generated Model'
+    street = models.BigIntegerField()
